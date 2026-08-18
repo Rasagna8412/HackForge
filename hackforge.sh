18d7c039071ec8383e5c7bfa2f06ca8e007b67d4
Rasagna Chowdary Kodali <rasagna8412@gmail.com>
2026-08-18 21:11:52 +0530

    Update hackforge.sh

--- hackforge.sh
+++ hackforge.sh
@@ -15,7 +15,7 @@ echo -e '\033[31;40;1m
 \033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m Rasagna
 '
 
-\e[37m[1]\e[36m Requirements & Update        \e[37m[2]\e[36m Phishing Tool              
+echo -e "\e[37m[1]\e[36m Requirements & Update        \e[37m[2]\e[36m Phishing Tool              
 \e[37m[3]\e[36m Webcam Hack                  \e[37m[4]\e[36m Subdomain Scanner          
 \e[37m[5]\e[36m Gmail Bomber                 \e[37m[6]\e[36m DDOS Attack            
 \e[37m[7]\e[36m How to use?                  \e[37m[8]\e[36m Uninstall downloaded programs      
@@ -23,9 +23,9 @@ echo -e '\033[31;40;1m
 \e[37m[11]\e[36m HackerPro                    \e[37m[12]\e[36m RED_HAWK
 \e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
 \e[37m[15]\e[36m BadMod                       \e[37m[16]\e[36m Facebash
-\e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m AUTO-IP-CHANGER
+\e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m AUTO-IP-CHANGER "
 
-'
+
 
 # Option Selection
 read -p "Enter option number: " choice
